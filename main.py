6aeab23a15958f55db939fde33bfa3dde6e758d1
julietabarrios93 <julietabarrios.cp@gmail.com>
2026-04-16 19:44:27 -0300

    Cambio codigo a numeros
    Se cambia nombre por suma de numeros

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-print("Hola mundo!")
+print("Hola mundo desde Main!")
 
-nombre = input("Bienvenido/a, ingrese su nombre: ")
+numero=int(input("Ingrese un numero: "))
+numero2=int(input("Ingrese otro numero: "))
 
-print (f"Hola {nombre}!!!!")
+print(f"Resultado de suma es {numero+numero2}")
